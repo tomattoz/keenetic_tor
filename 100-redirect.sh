56acd0f61423c62d08caec4776c174569b8bb8f3
Roman <bronner888@gmail.com>
2020-03-21 01:36:30 +0300

    Unblock Guest Network

--- 100-redirect.sh
+++ 100-redirect.sh
@@ -6,14 +6,17 @@
 if [ -z "$(iptables-save 2>/dev/null | grep unblock)" ]; then
     ipset create unblock hash:net -exist
     iptables -w -t nat -A PREROUTING -i br0 -p tcp -m set --match-set unblock dst -j REDIRECT --to-port 9141
+    iptables -w -t nat -A PREROUTING -i br1 -p tcp -m set --match-set unblock dst -j REDIRECT --to-port 9141
 fi
 
 if [ -z "$(iptables-save 2>/dev/null | grep "udp \-\-dport 53 \-j DNAT")" ]; then
     iptables -w -t nat -I PREROUTING -i br0 -p udp --dport 53 -j DNAT --to 192.168.1.1
+    iptables -w -t nat -I PREROUTING -i br1 -p udp --dport 53 -j DNAT --to 192.168.1.1
 fi
 
 if [ -z "$(iptables-save 2>/dev/null | grep "tcp \-\-dport 53 \-j DNAT")" ]; then
     iptables -w -t nat -I PREROUTING -i br0 -p tcp --dport 53 -j DNAT --to 192.168.1.1
+    iptables -w -t nat -I PREROUTING -i br1 -p tcp --dport 53 -j DNAT --to 192.168.1.1
 fi
 
 exit 0
